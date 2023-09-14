05d05570ff224cef271d7d10ecd08bd9eb92999f
Chandrashekhar Tripathi <tripathics@outlook.com>
2023-09-14 17:25:03 +0530

    revert Admin.js

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import TableHead from '@mui/material/TableHead';
+// import TablePagination from '@mui/material/TablePagination';
 import axios from 'axios';
+import Preview from './Preview';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-
 import { DataGrid } from '@mui/x-data-grid';
 import { Container } from '@mui/material';
-
 const columns = [
   { field: 'id', headerName: 'Sno', width: 40 },
   { field: 'fullname', headerName: 'Name', width: 160 },
@@ -16,7 +18,6 @@ const columns = [
   { field: 'email', headerName: 'Email', width: 200 },
   { field: 'phone', headerName: 'Phone', width: 120 },
   { field: 'location', headerName: 'Location', width: 100 }
-
   // {
   //   field: 'fullName',
   //   headerName: 'Full name',
@@ -27,7 +28,6 @@ const columns = [
   //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
   // },
 ];
-
 const rows = [
   { id: 1, fullname: 'Snow leaopartd', department: 'Computer Sciencce and Engineering', gradYear: '2024', current_org: 'google', current_desg: 'Software Engineer', email: 'rahulsah@google', phone: '70394958229', location: 'Banglore' },
   { id: 2, fullname: 'Lann leaopartdister', department: 'Computer Sciencce and Engineering', gradYear: '2024', current_org: 'google', current_desg: 'Software Engineer', email: 'rahulsah@google', phone: '70394958229', location: 'Banglore' },
@@ -42,16 +42,10 @@ const rows = [
   { id: 11, fullname: 'Clif leaopartdford', department: 'Computer Sciencce and Engineering', gradYear: '2024', current_org: 'google', current_desg: 'Software Engineer', email: 'rahulsah@google', phone: '70394958229', location: 'Banglore' },
   { id: 12, fullname: 'Fran leaopartdces', department: 'Computer Sciencce and Engineering', gradYear: '2024', current_org: 'google', current_desg: 'Software Engineer', email: 'rahulsah@google', phone: '70394958229', location: 'Banglore' },
   { id: 13, fullname: 'Roxi leaopartde', department: 'Computer Sciencce and Engineering', gradYear: '2024', current_org: 'google', current_desg: 'Software Engineer', email: 'rahulsah@google', phone: '70394958229', location: 'Banglore' },
-
-
 ];
-
-
 export default function Admin() {
   const [list, setList] = React.useState([]);
-  // eslint-disable-next-line no-unused-vars
   const [headings, setHeadings] = React.useState([]);
-
   const fetchAlumni = async () => {
     try {
       const res = await axios.get('/alumni')
@@ -60,7 +54,6 @@ export default function Admin() {
     catch (err) {
       console.log(err);
     }
-
   }
   React.useEffect(() => {
     if (list[0]) {
@@ -74,13 +67,10 @@ export default function Admin() {
       })
       setHeadings(headings);
     }
-
   }, [list])
-
   React.useEffect(() => {
     fetchAlumni();
   }, [])
-
   return (
     <Container maxWidth="xl">
       <DataGrid
@@ -94,7 +84,6 @@ export default function Admin() {
         pageSizeOptions={[5, 20]}
         checkboxSelection
       />
-
       <Stack direction="row" spacing={2}>
         <Button color="secondary">Update</Button>
         <Button variant="contained" color="success">
